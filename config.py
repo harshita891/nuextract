986739013412900881518e6ef2b5bacd4b4cb2bc
harshita891 <126278510+harshita891@users.noreply.github.com>
2025-04-12 23:33:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
 MODEL_NAME = "nuextract/model-base"
-INPUT_FILE = r"C:\Users\harsh\nlp\pdf_data_extracted_batch_1.json"
-OUTPUT_FILE = r"C:\Users\harsh\nlp\extracted_data.json"
+INPUT_FILE = 
+OUTPUT_FILE =
 USE_QUANTIZATION = True
